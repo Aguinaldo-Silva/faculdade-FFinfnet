747048d5d5e612997e566df8e13d0275894ea465
Aguinaldo Silva <aguinaldooo.ecio@gmail.com>
2024-08-01 22:12:13 -0300

    inicio projeto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./styles.css"
+
+
+export function App(){
+  return(
+    <header className="Container">
+      <h1> FF infnet</h1>
+
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+        <li>quem somos</li>
+      </ul>
+    </header>
+  )
+}
+
+
